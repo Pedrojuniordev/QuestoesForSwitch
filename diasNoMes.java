7e2ad6ea6ebda0fc2e558ebf95f32f36e812d2eb
Pedrojuniordev <pedrojunior44.pj@gmail.com>
2023-10-13 23:13:57 +0100

    quest 3

--- diasNoMes.java
+++ diasNoMes.java
@@ -1,9 +1,41 @@
 package questoes;
 
+import java.util.Scanner;
+
 public class diasNoMes {
 
-	public diasNoMes() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite um número do mês de 1 a 12: ");
+		int mes = scanner.nextInt();
+		
+		int diasNoMes = 0;
+		
+		switch (mes) {
+		case 1: // Janeiro
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			diasNoMes = 31;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			diasNoMes = 30;
+			break;
+		case 2:
+			diasNoMes = 28;
+			break;
+		default:
+			System.out.println("Mês inválido.");
+		}
+		
+		System.out.println("O mês " + mes + " tem "+ diasNoMes + " dias.");
 	}
 
 }
