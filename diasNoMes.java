f84f1170ffd0ed9328f7fb1e0208c18df08c38a6
Pedrojuniordev <pedrojunior44.pj@gmail.com>
2023-10-18 00:33:40 +0100

    caminhos Dinos

--- diasNoMes.java
+++ diasNoMes.java
@@ -14,17 +14,26 @@ public class diasNoMes {
 		
 		switch (mes) {
 		case 1: // Janeiro
+			break;
 		case 3:
+			break;
 		case 5:
+			break;
 		case 7:
+			break;
 		case 8:
+			break;
 		case 10:
+			break;
 		case 12:
 			diasNoMes = 31;
 			break;
 		case 4:
+			break;
 		case 6:
+			break;
 		case 9:
+			break;
 		case 11:
 			diasNoMes = 30;
 			break;
